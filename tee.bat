c8d8503755420e46616ce53688600efbe78d3f43
hidetoshi.mochizuki <gmochmoch@gmail.com>
2019-12-08 05:28:39 +0900

    tee修正

--- tee.bat
+++ tee.bat
@@ -4,4 +4,4 @@ rem =============================================
 rem                 generic tee
 rem =============================================
 set COMMAND=%*
-for /f "usebackq tokens=*" %%o in (`findstr .*`) do echo %%o | CScript.exe //NoLogo ~dp03rd-party\tee\Tee.VBS %COMMAND%
+for /f "usebackq tokens=*" %%o in (`findstr .*`) do echo %%o | CScript.exe //NoLogo %~dp03rd-party\tee\Tee.VBS %COMMAND%
