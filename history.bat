f0ac489dd8a0e21431483f828e426cc132ae7c97
Hidetoshi Mochizuki <hidetoshi.mochizuki@ulsystems.co.jp>
2019-12-09 12:29:05 +0900

    履歴表示バッチを追加

---
+++ history.bat
@@ -0,0 +1,9 @@
+@echo off
+setlocal
+rem =============================================
+rem                 show history
+rem ---------------------------------------------
+rem  usage:
+rem    history
+rem =============================================
+doskey /history
