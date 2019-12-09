051a6c952edbed0c410730470c7cc430c27e6133
Hidetoshi Mochizuki <hidetoshi.mochizuki@ulsystems.co.jp>
2019-12-09 10:39:10 +0900

    AMPM判定を簡略化

--- datetime.bat
+++ datetime.bat
@@ -5,6 +5,8 @@ rem                show datetime
 rem ---------------------------------------------
 rem  usage:
 rem    datetime [format]
+rem    # datetime yyyy/mm/dd hh:mi:ss
+rem    # 2019/12/09 10:36:54
 rem  format:
 rem    yyyy, y4    : four digits 'year'
 rem    yy, y2      : last tow digits 'year'
@@ -17,15 +19,15 @@ rem    mi          : 'minuts'
 rem    ss          : 'seconds'
 rem =============================================
 set "FORMAT=%*"
-set "NOW=%DATE% %TIME:~0,8%"
+set "NOW=%TIME: =0%"
+set "NOW=%DATE% %NOW:~0,8%"
 if "x%FORMAT%"=="x" echo %NOW%&exit /b
 set "H12=%NOW:~11,2%"
+set "AMPM=AM"
 if %H12% gtr 12 (
     set "AMPM=PM"
-    set /a H12=%H12%-12
-    if !H12! lss 10 set "H12=0!H12!"
-) else (
-    set "AMPM=AM"
+    set /a H12=1%H12%-12
+    set "H12=%H12:~1,2%"
 )
 set FORMAT=!FORMAT:YYYY=%NOW:~0,4%!
 set FORMAT=!FORMAT:Y4=%NOW:~0,4%!
