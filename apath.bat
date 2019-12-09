cdaba74be6df9608f767dec2b8c4d22b31b985cd
Hidetoshi Mochizuki <hidetoshi.mochizuki@ulsystems.co.jp>
2019-12-09 10:38:35 +0900

    第一引数をそのまま使用するようにした

--- apath.bat
+++ apath.bat
@@ -7,9 +7,8 @@ rem  usage:
 rem    apath [path]
 rem    echo [path]| apath
 rem =============================================
-set TPATH=%*
-if "x%TPATH%"=="x" (
+if "x%1"=="x" (
     for /f "usebackq tokens=*" %%i in (`findstr .*`) do echo %%~fi
 ) else (
-    for /f "usebackq tokens=*" %%i in (`echo %TPATH%`) do echo %%~fi
+    echo %~f1
 )
