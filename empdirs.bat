5154c718c5975bb6ac428a990f1b972df03c9fe8
Hidetoshi Mochizuki <hidetoshi.mochizuki@ulsystems.co.jp>
2019-12-11 15:58:50 +0900

    空フォルダ探索バッチ追加

---
+++ empdirs.bat
@@ -0,0 +1,13 @@
+@echo off
+setlocal
+rem =============================================
+rem             find empty directories
+rem ---------------------------------------------
+rem  usage:
+rem    empdirs [path]
+rem =============================================
+if not "x%1"=="x" ( set "CURRENT_DIR=%~fp1" ) else ( set "CURRENT_DIR=%~dp0" )
+for /r "%CURRENT_DIR%" /d %%i in ( . ) do (
+    dir /b "%%i" | findstr "^" >nul
+    if errorlevel 1 echo %%~fi
+)
