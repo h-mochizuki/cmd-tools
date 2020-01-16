f141785f6fa59556ec2ca315c6b4eb7bd55c455e
Hidetoshi Mochizuki <hidetoshi.mochizuki@ulsystems.co.jp>
2020-01-16 10:59:52 +0900

    cmd内でvbsを生成するように修正

--- bat/confirm.bat
+++ bat/confirm.bat
@@ -3,10 +3,40 @@ setlocal
 rem =============================================
 rem              show confirm dialog
 rem =============================================
+pushd %~pd0..
+set EL=1
+for %%i in ( "wk_%DATE:/=%%TIME:~0,2%%TIME:~3,2%%TIME:~6,2%%TIME:~9,2%" ) do (
+    mkdir "%%i"
+    pushd "%%i"
+    call :main %*
+    set "EL=!ERRORLEVEL!"
+    popd
+    rmdir /S /Q "%%i"
+)
+popd
+exit /b !EL!
+
+:main
+(
+    echo Option Explicit
+    echo Dim msg
+    echo Dim i
+    echo Dim re
+    echo If WScript.Arguments.Count ^< 1 Then
+    echo     msg = "OK?"
+    echo Else
+    echo     for i=1 to WScript.Arguments.Count
+    echo         If Len^(msg^) ^> 1 Then msg = msg ^& vbCrLf
+    echo         msg = msg ^& WScript.Arguments.Item^(i-1^)
+    echo     next
+    echo End If
+    echo WScript.Quit^(msgbox^(msg, vbOKCancel + vbQuestion^) - 1^)
+)>confirm.vbs
+
 if "x%1"=="x" (
     set MSG=
     for /f "usebackq tokens=*" %%i in (`findstr .*`) do set MSG=!MSG! "%%i"
-    CScript.exe //NoLogo %~pd0..\vbs\confirm.vbs !MSG!
+    CScript.exe //NoLogo confirm.vbs !MSG!
 ) else (
-    CScript.exe //NoLogo %~pd0..\vbs\confirm.vbs %*
+    CScript.exe //NoLogo confirm.vbs %*
 )
