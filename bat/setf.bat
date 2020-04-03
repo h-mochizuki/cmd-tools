81f540afea7908ee643c684a48e59e0534493ab7
Hidetoshi Mochizuki <hidetoshi.mochizuki@ulsystems.co.jp>
2020-04-03 10:53:36 +0900

    ダブルクォート対応

--- bat/setf.bat
+++ bat/setf.bat
@@ -7,6 +7,19 @@ rem    setf MSG=echo hello
 rem    # echo %MSG%
 rem    # hello
 rem =============================================
-for /f "tokens=1,* delims==" %%i in ( "%*" ) do (
-    for /f "usebackq tokens=*" %%o in ( `%%j` ) do set "%%i=%%o"
+for /f "usebackq" %%i in ( `argsize %*` ) do (
+    if "x%%i"=="x1" (
+        for /f "tokens=1,* delims==" %%i in ( %* ) do (
+            for /f "usebackq tokens=*" %%o in ( `%%j` ) do (
+                set "%%i=%%o"
+            )
+        )
+    ) else (
+        for /f "tokens=1,* delims==" %%i in ( "%*" ) do (
+            for /f "usebackq tokens=*" %%o in ( `%%j` ) do (
+                set "%%i=%%o"
+            )
+        )
+    )
 )
+
