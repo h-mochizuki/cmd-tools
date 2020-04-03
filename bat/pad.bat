15de321c6a0ebbde874857108e09890bb2675f91
Hidetoshi Mochizuki <hidetoshi.mochizuki@ulsystems.co.jp>
2020-04-03 09:43:55 +0900

    padのデフォルトを左寄せにした

--- bat/pad.bat
+++ bat/pad.bat
@@ -4,10 +4,10 @@ rem =============================================
 rem         Pad the String with the spaces
 rem ---------------------------------------------
 rem  usage:
-rem    pad 1234 10
-rem    # 0000001234
-rem    pad hoge -10 *
+rem    pad hoge 10 *
 rem    # hoge******
+rem    pad 1234 -10 0
+rem    # 0000001234
 rem =============================================
 
 set "WC=0"
@@ -15,11 +15,11 @@ set WD=%~1
 for /f "usebackq" %%i in ( `strlen %WD%` ) do set "WC=%%i"
 if %WC% LEQ 0 exit /b
 
-set "REV="
+set "RIGHT="
 set SIZE=%~2
 if "x%SIZE%"=="x" set "SIZE=%WC%"
 if "x%SIZE:~0,1%"=="x-" (
-    set "REV=1"
+    set "RIGHT=1"
     set "SIZE=!SIZE:~1!"
 )
 if %SIZE% LEQ 0 set "SIZE=%WC%"
@@ -30,8 +30,8 @@ if "x%FC%"=="x" set "FC= "
 
 set "RE="
 for /l %%i in (1,1,%TO%) do set "RE=!RE!!FC!"
-if "x%REV%"=="x" (
-    echo !RE!!WD!
-) else (
+if "x%RIGHT%"=="x" (
     echo !WD!!RE!
+) else (
+    echo !RE!!WD!
 )
