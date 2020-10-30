0cde50d2672aaafe14fa833aad0349380cd95b2d
Hidetoshi Mochizuki <hidetoshi.mochizuki@ulsystems.co.jp>
2020-10-30 15:38:29 +0900

    デバッグに対応

--- bat/jws.bat
+++ bat/jws.bat
@@ -1,9 +1,16 @@
 @echo off
-setlocal
+setlocal EnableDelayedExpansion
 rem =============================================
 rem                 run javaws
 rem =============================================
 if "x%1"=="x" goto :usage
+if "x%JAVA_HOME%"=="x" (
+    where java >nul 2>&1
+    if errorlevel 1 echo Java is not installed >&2 & exit /b 1
+    for /f "usebackq tokens=*" %%i in ( `where java` ) do set "JAVA_BIN=%%~dpi"
+    for %%i in ( "!JAVA_BIN!." ) do set "JAVA_HOME=%%~dpi"
+    set JAVA_HOME=!JAVA_HOME:~0,-1!
+)
 if "x%1"=="x-c" goto :control_panel
 if "x%1"=="x/c" goto :control_panel
 goto :run_jws
@@ -18,20 +25,16 @@ echo   -c : open java control panel
 exit /b 1
 
 :control_panel
-javacpl >nul 2>&1
-if not errorlevel 1 exit /b
-for /f "usebackq tokens=*" %%i in ( `where java` ) do set "JAVA_BIN=%%~dpi"
-pushd "%JAVA_BIN%"
-for %%i in ( "." ) do set "JAVA_HOME=%%~dpi"
-if exist "%JAVA_HOME%jre" set "JAVA_HOME=%JAVA_HOME%jre"
-pushd "%JAVA_HOME%\bin"
-javacpl
+if exist "%JAVA_HOME%\jre" set "JAVA_HOME=%JAVA_HOME%\jre"
+"%JAVA_HOME%\bin\javacpl"
 exit /b
 
 :run_jws
+set JAVAWS_TRACE_NATIVE=1
+set JAVA_TOOL_OPTIONS=-agentlib:jdwp=transport=dt_socket,address=12345,server=y,suspend=n %JAVA_TOOL_OPTIONS%
 echo %*|findstr "^-verbose">nul 2>&1
 if not errorlevel 1 (
-    javaws %*
+    "%JAVA_HOME%\bin\javaws" %*
 ) else (
-    javaws -verbose %*
+    "%JAVA_HOME%\bin\javaws" -verbose %*
 )
