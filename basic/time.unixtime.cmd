6bab9e526391771c9d44d8e78f06ad668db279b5
h-mochizuki <gmochmoch@gmail.com>
2021-02-21 05:11:17 +0900

    unixtime表示コマンドを追加

---
+++ basic/time.unixtime.cmd
@@ -0,0 +1,15 @@
+@if(0)==(0) echo off
+setlocal
+if not "x%~1"=="x/?" goto :main
+echo =============================================
+echo         Return current unixtime.
+echo ---------------------------------------------
+echo  usage:
+echo    time.unixtime -^> 16138...
+echo =============================================
+exit /b
+:main
+call cscript //nologo /E:JScript "%~f0" %*
+exit /b
+@end
+WScript.Echo(new Date().getTime());
