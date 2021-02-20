3499fbe91d19667ff75e8673befdb9873dae0011
h-mochizuki <gmochmoch@gmail.com>
2021-02-21 05:11:02 +0900

    先頭を除いた引数を返すコマンドを追加

---
+++ basic/arg.remains.cmd
@@ -0,0 +1,30 @@
+@echo off
+setlocal
+if not "x%~1"=="x/?" goto :main
+echo =============================================
+echo   Return arguments without first argument.
+echo ---------------------------------------------
+echo  usage:
+echo    arg.remains 1 2 "3 4" 5 -^> 2 "3 4" 5
+echo  args:
+echo    1+: arguments
+echo =============================================
+exit /b
+:main
+shift
+set "result="
+
+:next
+set "arg=%~1"
+if "x%arg%"=="x" (
+    echo %result:__{double_quote}__="%
+    exit /b
+)
+echo "%arg%"| findstr /C:" " >nul 2>&1
+if errorlevel 1 (
+    set "result=%result%%arg% "
+) else (
+    set "result=%result%__{double_quote}__%arg%__{double_quote}__ "
+)
+shift
+goto :next
