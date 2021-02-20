793f5be6999dfcacec2a38b0fc583c97e88daf89
h-mochizuki <gmochmoch@gmail.com>
2021-02-21 05:55:05 +0900

    確認ダイアログ表示コマンド追加

---
+++ basic/dialog.confirm.cmd
@@ -0,0 +1,40 @@
+@if(0)==(0) echo off
+setlocal
+if not "x%~1"=="x/?" goto :main
+echo =============================================
+echo             Show confirm dialog.
+echo ---------------------------------------------
+echo  usage:
+echo    dialog.confirm "Are you sure?"
+echo    #if errorlevel 1 exit /b 1
+echo  args:
+echo    1: input-string or input-stream
+echo; 
+echo  caution: argment is required.
+echo    dialog.confirm -^> FREEZE!! WAIT INPUT.
+echo =============================================
+exit /b
+:main
+if "x%~1"=="x" (
+    findstr .* | call cscript //nologo /E:JScript "%~f0"
+) else (
+    echo %* | call cscript //nologo /E:JScript "%~f0"
+)
+exit /b %errorlevel%
+@end
+var text = "";
+while (!WScript.StdIn.AtEndOfStream) {
+    if (text != "") {
+        text = text + "\n";
+    }
+    text = text + WScript.StdIn.ReadLine(); 
+}
+if (text == "") {
+    text = "Are you sure?";
+}
+var sh = WScript.CreateObject("WScript.Shell");
+var ret = sh.Popup(text, 0, "Windows Script Host", 32+4+0)
+if (ret == 6) {
+    WScript.Quit(0);
+}
+WScript.Quit(1);
