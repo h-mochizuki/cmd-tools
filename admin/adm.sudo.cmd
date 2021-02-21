84fac6251d82ac0df555b41aebe614bec1c10ed4
h-mochizuki <gmochmoch@gmail.com>
2021-02-22 00:43:21 +0900

    複数コマンド、パイプに対応

--- admin/adm.sudo.cmd
+++ admin/adm.sudo.cmd
@@ -9,20 +9,31 @@ echo    adm.sudo echo abc -^> abc
 echo  args:
 echo    1+: commands
 echo;
-echo  caution:
+echo  caution1:
 echo    command execute in another prompt,
 echo    if you are not administrator.
+echo  caution2:
+echo    "`l" use instead of pipe.
+echo    adm.sudo echo abc `l findstr a
+echo  caution3:
+echo    "`n" use instead of line feed.
+echo    adm.sudo echo abc `n pause
 echo =============================================
 exit /b
 :main
 where assert.admin >nul 2>nul
 if errorlevel 1 set "PATH=%PATH%;%~dp0;%~dp0..\basic"
 
-call assert.admin
-if not errorlevel 1 (
-    %*
-    exit /b %errorlevel%
-)
 set "command=%*"
+call assert.admin
+if not errorlevel 1 goto :admin
+set "command=%command:`n=^&%"
+set "command=%command:`l=^|%"
 set "command=%command:"=\"%"
 powershell Start-Process -verb runAs cmd -ArgumentList '/c ""cd /d %cd%^&%command%""'
+exit /b
+
+:admin
+set "command=%command:`n=&%"
+set "command=%command:`l=|%"
+%command%
