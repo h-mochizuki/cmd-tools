7802e927e31d31c053c5dcbba06039796094a0c8
h-mochizuki <gmochmoch@gmail.com>
2021-02-22 00:42:27 +0900

    拡張子を指定できるように修正

--- basic/file.temp.cmd
+++ basic/file.temp.cmd
@@ -8,14 +8,17 @@ echo  usage:
 echo    file.temp -^> %%temp%%\bat~123.tmp
 echo  args:
 echo    1: folder path [create %%temp%% when empty]
+echo    2: postfix [append tmp when empty]
 echo =============================================
 exit /b
 :main
 set "tmpdir=%tmp%"
 if not "x%~1"=="x" set "tmpdir=%~1"
 mkdir "%tmpdir%">nul 2>&1
+set "postfix=tmp"
+if not "x%~2"=="x" set "postfix=%~2"
 
 :mktmp
-set "tmpfile=%tmpdir%\bat~%random%.tmp"
+set "tmpfile=%tmpdir%\bat~%random%.%postfix%"
 if exist "%tmpfile%" goto :mktmp
 echo %tmpfile%
