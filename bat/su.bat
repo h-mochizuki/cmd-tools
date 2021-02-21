02f5a301e4d54057814666189434839e2265407b
h-mochizuki <gmochmoch@gmail.com>
2021-02-22 01:11:35 +0900

    再作成したコマンドを削除

--- bat/su.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-setlocal
-rem =============================================
-rem        swhich administrator role
-rem =============================================
-openfiles >nul 2>&1
-if errorlevel 1 powershell start-process cmd -ArgumentList '/k ""cd /d %CD%""' -verb runas
--- bat/tee.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-setlocal
-rem =============================================
-rem                 generic tee
-rem =============================================
-set COMMAND=%*
-for /f "usebackq tokens=*" %%o in (`findstr .*`) do echo %%o | CScript.exe //NoLogo %~pd0..\3rd-party\tee\Tee.VBS %COMMAND%
