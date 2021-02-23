332cc28b33e2906af78cfb4f64673ee9e36a8192
h-mochizuki <gmochmoch@gmail.com>
2021-02-23 21:42:00 +0900

    hostsの内容を表示するコマンドを追加

---
+++ bin/hosts.cmd
@@ -0,0 +1,14 @@
+@echo off
+setlocal
+if not "x%~1"=="x/?" goto :main
+echo =============================================
+echo                 Show hosts.
+echo ---------------------------------------------
+echo  usage:
+echo    hosts
+echo =============================================
+exit /b 1
+:main
+set "hosts=C:\Windows\System32\drivers\etc\hosts"
+type %hosts%| findstr /V /R "#.*$"| findstr /V /R "^$"
+exit /b 0
