6438ca4ff05a5f35041aa2d801f4c8ed4f3a29ae
h-mochizuki <gmochmoch@gmail.com>
2021-02-23 11:38:23 +0900

    キーが空の場合には処理しないように修正

--- basic/var.loadenv.cmd
+++ basic/var.loadenv.cmd
@@ -4,14 +4,16 @@ echo =============================================
 echo        Load variables from env file.
 echo ---------------------------------------------
 echo  usage:
-echo    var.loadenv file
+echo    var.loadenv [file]
 echo  args:
-echo    1: environment file path
+echo    1: file path
 echo =============================================
 exit /b
 :main
 if "x%~1"=="x" exit /b
 for /f "eol=# tokens=1,* delims==" %%i in ( %~f1 ) do (
-    set "%%i=%%j"
-    echo "%%i=%%j"
+    if not "x%%i"=="x" (
+        set "%%i=%%j"
+        echo "%%i=%%j"
+    )
 )
