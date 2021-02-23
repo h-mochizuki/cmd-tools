baca58a9ab46334bda71e788e7b58109a5ee7e63
h-mochizuki <gmochmoch@gmail.com>
2021-02-23 20:54:04 +0900

    パイプとアンドのエスケープに対応

--- basic/arr.each.cmd
+++ basic/arr.each.cmd
@@ -11,11 +11,18 @@ echo    echo a^| arr.each echo {}{} -^> aa
 echo  args:
 echo    1: command [{} replaced stream item]
 echo;
-echo  caution: input-stream required.
-echo    arr.each -^> FREEZE!! WAIT INPUT.
+echo  caution1:
+echo    input-stream required.
+echo    ex) arr.each -^> FREEZE!! WAIT INPUT.
+echo  caution2:
+echo    have to escape like "^|" or "^&"
+echo    if use "|" or "&".
+echo    ex) echo abc^| arr.each echo {}{}^^^| findstr a
 echo =============================================
 exit /b 1
 :main
-set "command=%*"
 set "replacer={}"
+set "command=%*"
+set "command=%command:&=^&%"
+set "command=%command:|=^|%"
 for /f "usebackq tokens=*" %%o in (`findstr .*`) do call %%command:%replacer%=%%o%%
