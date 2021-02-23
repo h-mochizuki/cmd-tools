46b34f7aa9f56570f63ec444cd7c2155a38c388f
h-mochizuki <gmochmoch@gmail.com>
2021-02-24 00:03:31 +0900

    コマンドが使えるか判定するコマンドを追加

---
+++ bin/path_available.cmd
@@ -0,0 +1,26 @@
+@echo off
+setlocal
+if not "x%~1"=="x/?" goto :main
+echo =============================================
+echo          Check command is available.
+echo ---------------------------------------------
+echo  usage:
+echo    path_available [command]
+echo  ex:
+echo    path_available java
+echo    if errorlevel 1 echo unavailable -^> unavailable
+echo  args:
+echo    1: command
+echo  return code:
+echo    0: available
+echo    1: unavailable
+echo =============================================
+exit /b 1
+:main
+if "x%~1"=="x" exit /b 0
+where "%~1" >nul 2>&1
+if errorlevel 1 (
+    echo '%~1' is unavailable
+    exit /b 1
+)
+exit /b 0
