f213efd986122959a84a2407f8ae540dd4e9e304
h-mochizuki <gmochmoch@gmail.com>
2021-02-24 14:12:47 +0900

    PATH内容をリストにして表示するコマンドを追加

---
+++ bin/path_list.cmd
@@ -0,0 +1,20 @@
+@echo off
+setlocal
+if not "x%~1"=="x/?" goto :main
+echo =============================================
+echo           Show separated PATH list.
+echo ---------------------------------------------
+echo  usage:
+echo    path_list
+echo =============================================
+exit /b 1
+:main
+for /f "usebackq tokens=1,* delims==" %%o in (`path`) do call :print "%%~p"
+exit /b
+
+:print
+if "x%~1"=="x" exit /b
+for /f "tokens=1,* delims=;" %%o in ( "%~1" ) do (
+    echo %%~o
+    call :print "%%~p"
+)
