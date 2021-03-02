8d4dcb4b02fa522ecc8af9827a2612543f974620
h-mochizuki <gmochmoch@gmail.com>
2021-03-02 10:23:59 +0900

    hostsを編集できるようにした

--- bin/hosts.cmd
+++ bin/hosts.cmd
@@ -8,9 +8,16 @@ echo  usage:
 echo    hosts [option]
 echo  options:
 echo\   /?: show this usage.
+echo    /e: edit /etc/hosts
 echo =============================================
 exit /b 1
 :main
 set "hosts=C:\Windows\System32\drivers\etc\hosts"
+if "x%~1"=="x/e" goto :edit
+if "x%~1"=="x-e" goto :edit
 type %hosts%| findstr /V /R "#.*$"| findstr /V /R "^$"
 exit /b 0
+
+:edit
+call %~dp0path_include
+call adm_sudo notepad "%hosts%"
